5452a14d1b26718d3b53d6b4f3a8325b7c3c6813
Quang <qtran1018@gmail.com>
2025-02-05 05:16:32 -0500

    Changed message method to accept params for message.

--- export_model.py
+++ export_model.py
@@ -4,7 +4,7 @@ import os
 import tkinter as tk
 from tkinter import messagebox
 
-def show_message():
+def show_message(popup_message):
     # Create a Tk root widget
     root = tk.Tk()
     # Hide the root window
@@ -12,7 +12,7 @@ def show_message():
     # Display a message box
     messagebox.showinfo(
         title="Information",
-        message="Model has been saved."
+        message=popup_message
     )
 
 def export_model(save_loc,data_loc):
@@ -20,4 +20,4 @@ def export_model(save_loc,data_loc):
     model = linear_model_maker.make_model(data_loc)
     filepath = os.path.join(location,'weather_model.pkl')
     joblib.dump(model,filepath)
-    show_message()
+    show_message("Model has been saved.")
